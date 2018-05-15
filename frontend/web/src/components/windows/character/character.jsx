f0bf853902c5d7c9f9608bbb576cbf7b45afabf1
Ray <110681032@qq.com>
2018-05-15 23:26:55 +0800

    init: backend service

--- frontend/web/src/components/windows/character/character.jsx
+++ frontend/web/src/components/windows/character/character.jsx
@@ -42,6 +42,40 @@ class Character extends Component {
 	}
 }
 
+Character.attributes = {
+	value:{
+		health: '生命',
+		spirit: '精神',
+	},
+	basic: {
+		strength: '力量',
+		agility: '敏捷',
+		dexterity: '技巧',
+		stamina: '耐力',
+		mind: '念力',
+		experience: '经验',
+		intelligence: '智慧',
+	},
+	advanced: {
+		speed: '速度', //敏捷 耐力 
+		movement: '移动',// 力量 敏捷 耐力
+		accurancy: '精准',//敏捷 技巧 经验
+		dodge: '闪避',//敏捷 力量 经验
+		defense: '防御',//力量 耐力 
+		damage: '破坏',//力量 敏捷 技巧 经验
+		resistance: '阻力',//念力 精神
+		bearing: '负重',//力量 耐力
+		learning: '学习'//智慧 经验 精神
+	},
+	power: {
+		burnning: '燃烧',//念力 精神 智慧
+		voltage: '电压',//念力 精神 智慧
+		freeze: '冻结',//念力 精神 智慧
+		telekinetic: '念动力'//念力 精神 智慧
+	}
+
+}
+
 const mapStateToProps = (state, props) => {
 
 	return {
