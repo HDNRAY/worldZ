f897e560e5e7bdc0f17dbb5c728483efc90329bc
Ray <110681032@qq.com>
2018-05-20 19:13:40 +0800

    opt: MovePaths

---
+++ frontend/web/src/services/battle/movePaths.js
@@ -0,0 +1,75 @@
+'user strict'
+
+module.exports = class MovePaths {
+
+    constructor(origin, movement) {
+        this.origin = origin
+        this.movement = movement
+        this.unreachables = []
+        this.reachables = null
+    }
+
+    setTerrains(terrains) {
+        this.unreachables.push(terrains)
+
+        return this
+    }
+
+    setEnemies(enemies) {
+        this.unreachables.push(enemies)
+
+        return this
+    }
+
+    getReachables() {
+
+        const reachables = new Map()
+        // let lastPoints = [this.origin]
+        let lastSteps = [this.origin]
+        for (let step = 0; step < this.movement; step++) {
+            lastSteps = lastSteps.reduce((nextSteps, lastStep) => {
+                let nextPointDirections = [[0, -1], [-1, 0], [1, 0], [0, 1]]
+                nextPointDirections = lastStep.y % 2 ? [...nextPointDirections, [1, 1], [1, -1]] : [...nextPointDirections, [-1, 1], [-1, -1]]
+
+                nextPointDirections.map(nextPointDirection => {
+                    let nextStep = {
+                        x: lastStep.x + nextPointDirection[0],
+                        y: lastStep.y + nextPointDirection[1],
+                    };
+                    if (!reachables.has(nextStep)) {
+                        nextSteps.push(nextStep);
+                        reachables.set(nextStep, lastStep);
+                    }
+                })
+
+                return nextSteps
+            }, [])
+        }
+
+        this.reachables = reachables
+        return this.reachables
+    }
+
+    
+
+    getPaths(destination) {
+        if(!destination){
+            console.log('destination is invalid')
+            return
+        }
+        if (!this.reachables) this.reachables = this.getReachables()
+
+        const findLastStep = (step, path) => {
+            path.unshift(step)
+            if (step === this.origin) {
+                return path
+            }
+
+            step = this.reachables.get(step)
+            return findLastStep(step, path)
+        }
+
+        return findLastStep(destination, [])
+    }
+
+}
--- frontend/web/src/services/battle/test.js
+++ frontend/web/src/services/battle/test.js
@@ -1,33 +1,9 @@
-const battle = require('./battle');
-let state = {
-	characters: [{
-		name: 'Ray',
-		attributes: {
-			atk_period: 15,
-			attack: 5,
-			health: 20
-		},
-		skills: {
-			active: [],
-			passive: []
-		},
-		team: 0,
-		alive: 1
-	}, {
-		name: '61f',
-		attributes: {
-			atk_period: 10,
-			attack: 3,
-			health: 20
-		},
-		skills: {
-			active: [],
-			passive: []
-		},
-		team: 1,
-		alive: 1
-	}]
-}
+const MovePaths = require('./movePaths')
 
-console.log(state)
-battle.compute(state)
+const movePaths = new MovePaths({ x: 10, y: 10 }, 2)
+
+console.log('reachables', movePaths.getReachables())
+const reachables = movePaths.getReachables();
+for (let key of reachables.keys()){
+	console.log(key,':', movePaths.getPaths(key))
+}
